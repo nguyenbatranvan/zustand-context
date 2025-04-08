8970d1499114a6b7b467b25f7fa1f89bbb2c5445
Van Nguyen <nguyenbatranvan96@gmail.com>
2025-04-08 09:05:04 +0700

    change create context

--- src/features/context-example/zustand-context/provider/user-zustand-context.tsx
+++ src/features/context-example/zustand-context/provider/user-zustand-context.tsx
@@ -1,10 +1,10 @@
 import {createStore, StoreApi} from "zustand/vanilla";
 import {TParameterContext, UserState} from "@/features/context-example/context.types.ts";
 import {subscribeWithSelector} from "zustand/middleware";
-import {FC, useMemo} from "react";
-import {createContext, useContextSelector} from "use-context-selector";
+import {createContext, FC, useContext, useMemo} from "react";
 import {useStore} from "zustand/react";
-
+// https://zustand.docs.pmnd.rs/migrations/migrating-to-v4#createcontext
+// https://zustand.docs.pmnd.rs/previous-versions/zustand-v3-create-context#createcontext-from-zustand/context
 const UserContextZustand = createContext<StoreApi<UserState> | null>(null);
 const createUserStore = () =>
     createStore<UserState>()(subscribeWithSelector((set) => ({
@@ -23,13 +23,13 @@ export const UserProviderZustand: FC<TParameterContext> = ({children}) => {
 };
 
 export const useUserContextZustand = <TSelected = any>(selector: (data: UserState) => TSelected) => {
-    const store = useContextSelector(UserContextZustand, (context) => context);
+    const store = useContext(UserContextZustand);
     if (!store) throw new Error('UserContextZustand must be used within a UserProviderZustand');
     return useStore(store, selector);
 };
 
 export const useUserStoreZustand = () => {
-    const store = useContextSelector(UserContextZustand, (context) => context);
+    const store = useContext(UserContextZustand);
     if (!store) throw new Error('UserContextZustand must be used within a UserProviderZustand');
     return store;
 };
